9583f4604afc9449a1417c405a9d5ee10b59a724
Konrad Grzanek <kongra@gmail.com>
2014-11-14 23:32:14 +0100

    Added AI.Rete.Print module.

---
+++ AI/Rete/Print.hs
@@ -0,0 +1,18 @@
+{-# LANGUAGE CPP #-}
+#if defined(__GLASGOW_HASKELL__) && __GLASGOW_HASKELL__ >= 702
+{-# LANGUAGE Trustworthy #-}
+#endif
+{-# OPTIONS_GHC -W -Wall #-}
+------------------------------------------------------------------------
+-- |
+-- Module      : AI.Rete.Print
+-- Copyright   : (c) 2014 Konrad Grzanek
+-- License     : BSD-style (see the file LICENSE)
+-- Created     : 2014-11-14
+-- Maintainer  : kongra@gmail.com
+-- Stability   : experimental
+-- Portability : requires stm
+--
+-- Textual visualization of Rete network and data.
+------------------------------------------------------------------------
+module AI.Rete.Print where

d06bfa9a5bd76f4675526f5f386ca42da27bceaf
Konrad Grzanek <kongra@gmail.com>
2015-03-12 10:17:41 +0100

    Re-implemented creating Bmems and Joins.

--- AI/Rete/Net.hs
+++ AI/Rete/Net.hs
@@ -13,14 +13,12 @@ module AI.Rete.Net where
 import           AI.Rete.Data
 import           AI.Rete.Flow
 import           AI.Rete.State
-import           Control.Monad (liftM)
+import           Control.Monad (liftM, forM_)
 import qualified Data.HashMap.Strict as Map
 import qualified Data.HashSet as Set
+import           Data.Maybe (isJust, fromJust)
 import           Kask.Control.Lens
-
--- import           Control.Monad (liftM, forM_)
--- import           Data.Maybe (isJust, fromJust)
--- import           Safe (headMay)
+import           Safe (headMay)
 
 -- CREATING ALPHA MEMORY AND FEEDING IN INITIAL Wmes.
 
@@ -35,9 +33,9 @@ buildOrShareAmem (Obj o) (Attr a) (Val v) = do
       o'  = Obj  (f o)
       a'  = Attr (f a)
       v'  = Val  (f v)
+      k   = Wme o' a' v'
 
   amems <- liftM (view reteAmems) (viewS ())
-  let k = Wme o' a' v'
 
   case Map.lookup k amems of
     Just amem -> return amem -- Happily found.
@@ -122,129 +120,104 @@ wmesForAmemFeed True True True rete _ _ _ =
   view reteWmes rete
 {-# INLINE wmesForAmemFeed #-}
 
--- -- BETA MEMORY CREATION
+-- BETA MEMORY CREATION
 
--- buildOrShareBmem :: Join -> ReteM Bmem
--- buildOrShareBmem join = do
---   rete <- get
---   let joinState = Map.lookupDefault
---                   (error ("PANIC (10): STATE NOT FOUND FOR " ++ show join))
---                   join (reteJoinStates rete)
+buildOrShareBmem :: Join -> ReteM Bmem
+buildOrShareBmem parent = do
+  -- Search for existing Bmem to share.
+  parentState <- viewS parent
+  case view joinChildBmem parentState of
+    Just bmem -> return bmem  -- Happily found.
+    Nothing   -> do
+      -- Let's create a new Bmem and bind it to its state.
+      bmem <- liftM Bmem genid
+      overS (over reteBmemStates (Map.insert bmem (BmemState [] []))) ()
 
---   case joinChildBmem joinState of
---     Just bmem -> return bmem  -- Happily found.
---     Nothing   -> do
---       -- Let's create new Bmem.
---       bmem <- liftM Bmem genid
---       let bmemState = BmemState [] []
+      -- Update with matches from above:
+      -- 1. Set bmem as a single child of parent (join).
+      setS parent $ JoinState (Just bmem) []
 
---       -- Update with matches from above:
---       -- 1. Set bmem as singleton child of join.
---       rete1 <- get
---       let joinState1 = JoinState { joinChildBmem  = Just bmem
---                                  , joinChildProds = [] }
---       put rete1 { reteBmemStates = Map.insert bmem bmemState
---                                    (reteBmemStates rete1)
---                 , reteJoinStates = Map.insert join joinState1
---                                    (reteJoinStates rete1) }
+      -- 2. Right-activate parent
+      amemState <- viewS (joinAmem parent)
+      forM_ (view amemWmes amemState) $ \wme -> rightActivateJoin wme parent
 
---       -- 2. Right-activate-parent.
---       let amem       = joinAmem join
---           amemStates = reteAmemStates rete1
---           amemState  = Map.lookupDefault
---                        (error ("PANIC (11): STATE NOT FOUND FOR " ++ show amem))
---                        amem amemStates
---       forM_ (amemWmes amemState) $ \wme -> rightActivateJoin wme join
-
---       -- 3. Restore new children and put all in place.
---       rete2 <- get
---       let joinState2  = JoinState { joinChildBmem  = Just bmem
---                                   , joinChildProds = joinChildProds joinState }
---       put rete2 { reteJoinStates = Map.insert join joinState2
---                                    (reteJoinStates rete2) }
---       return bmem
--- {-# INLINE buildOrShareBmem #-}
+      -- 3. Restore parent state with bmem inside.
+      setS parent $ JoinState (Just bmem) (view joinChildProds parentState)
+      return bmem
+{-# INLINE buildOrShareBmem #-}
 
 -- -- JOIN TESTS
 
--- type IndexedCond = (Int, Cond)
+type IndexedCond = (Int, Cond)
 
--- indexedConds :: [Cond] -> [IndexedCond]
--- indexedConds = zip [0 ..]
--- {-# INLINE indexedConds #-}
+indexedConds :: [Cond] -> [IndexedCond]
+indexedConds = zip [0 ..]
+{-# INLINE indexedConds #-}
 
--- fieldEqualTo :: Cond -> ConstantOrVariable -> Maybe Field
--- fieldEqualTo (Cond (Obj o) (Attr a) (Val v)) s
---   | o == s    = Just O
---   | a == s    = Just A
---   | v == s    = Just V
---   | otherwise = Nothing
--- {-# INLINE fieldEqualTo #-}
+fieldEqualTo :: Cond -> ConstantOrVariable -> Maybe Field
+fieldEqualTo (Cond (Obj o) (Attr a) (Val v)) s
+  | o == s    = Just O
+  | a == s    = Just A
+  | v == s    = Just V
+  | otherwise = Nothing
+{-# INLINE fieldEqualTo #-}
 
--- matchingLocation :: ConstantOrVariable -> IndexedCond -> Maybe Location
--- matchingLocation s (i, cond) = case fieldEqualTo cond s of
---   Nothing -> Nothing
---   Just f  -> Just (Location i f)
--- {-# INLINE matchingLocation #-}
+matchingLocation :: ConstantOrVariable -> IndexedCond -> Maybe Location
+matchingLocation s (i, cond) = case fieldEqualTo cond s of
+  Nothing -> Nothing
+  Just f  -> Just (Location i f)
+{-# INLINE matchingLocation #-}
 
--- joinTestForField :: Int -> ConstantOrVariable -> Field -> [IndexedCond]
---                  -> Maybe JoinTest
--- joinTestForField i v field earlierConds =
---   case v of
---     JustVariable _ -> case headMay (matches earlierConds) of
---       Nothing              -> Nothing
---       Just (Location i' f) -> Just (JoinTest field f (i - i' - 1))
+joinTestForField :: Int -> ConstantOrVariable -> Field -> [IndexedCond]
+                 -> Maybe JoinTest
+joinTestForField i v field earlierConds =
+  case v of
+    JustVariable _ -> case headMay (matches earlierConds) of
+      Nothing              -> Nothing
+      Just (Location i' f) -> Just (JoinTest field f (i - i' - 1))
 
---     JustConstant _ -> Nothing -- No tests from Consts (non-Vars).
---   where
---     matches = map fromJust . filter isJust . map (matchingLocation v)
--- {-# INLINE joinTestForField #-}
+    JustConstant _ -> Nothing -- No tests from Consts (non-Vars).
+  where
+    matches = map fromJust . filter isJust . map (matchingLocation v)
+{-# INLINE joinTestForField #-}
 
--- joinTestsForCondImpl :: Int
---                      -> Obj  ConstantOrVariable
---                      -> Attr ConstantOrVariable
---                      -> Val  ConstantOrVariable
---                      -> [IndexedCond] -> [JoinTest]
--- joinTestsForCondImpl i (Obj o) (Attr a) (Val v) earlierConds =
---   result3
---   where
---     test1   = joinTestForField i o O earlierConds
---     test2   = joinTestForField i a A earlierConds
---     test3   = joinTestForField i v V earlierConds
+joinTestsForCondImpl :: Int
+                     -> Obj  ConstantOrVariable
+                     -> Attr ConstantOrVariable
+                     -> Val  ConstantOrVariable
+                     -> [IndexedCond] -> [JoinTest]
+joinTestsForCondImpl i (Obj o) (Attr a) (Val v) earlierConds =
+  result3
+  where
+    test1   = joinTestForField i o O earlierConds
+    test2   = joinTestForField i a A earlierConds
+    test3   = joinTestForField i v V earlierConds
 
---     result1 = [fromJust test3 | isJust test3]
---     result2 = if isJust test2 then fromJust test2 : result1 else result1
---     result3 = if isJust test1 then fromJust test1 : result2 else result2
--- {-# INLINE joinTestsForCondImpl #-}
+    result1 = [fromJust test3 | isJust test3]
+    result2 = if isJust test2 then fromJust test2 : result1 else result1
+    result3 = if isJust test1 then fromJust test1 : result2 else result2
+{-# INLINE joinTestsForCondImpl #-}
 
--- joinTestsForCond :: IndexedCond -> [IndexedCond] -> [JoinTest]
--- joinTestsForCond (i, Cond o a v) = joinTestsForCondImpl i o a v
--- {-# INLINE joinTestsForCond #-}
+joinTestsForCond :: IndexedCond -> [IndexedCond] -> [JoinTest]
+joinTestsForCond (i, Cond o a v) = joinTestsForCondImpl i o a v
+{-# INLINE joinTestsForCond #-}
 
--- -- JOIN CREATION
+-- JOIN CREATION
 
--- buildOrShareJoin :: Bmem -> Amem -> [JoinTest] -> ReteM Join
--- buildOrShareJoin bmem amem tests = do
---   -- Search for an existing Join to share.
---   rete <- get
---   let bmemState = Map.lookupDefault
---                   (error ("PANIC (12): STATE NOT FOUND FOR " ++ show bmem))
---                   bmem (reteBmemStates rete)
---       isMatchingJoin j = amem == joinAmem j && tests == joinTests j
+buildOrShareJoin :: Bmem -> Amem -> [JoinTest] -> ReteM Join
+buildOrShareJoin parent amem tests = do
+  -- Search for an existing Join to share.
+  parentState <- viewS parent
+  let isMatchingJoin j = amem == joinAmem j && tests == joinTests j
+  case headMay (filter isMatchingJoin (view bmemChildren parentState)) of
+    Just join -> return join  -- Happily found.
+    Nothing   -> do
+      -- Let's create a new Join and bind it to its state.
+      i <- genid
+      let join = Join i tests amem parent
+      overS (over reteJoinStates (Map.insert join (JoinState Nothing []))) ()
 
---   case headMay (filter isMatchingJoin (bmemChildren bmemState)) of
---     Just join -> return join  -- Happily found.
---     Nothing   -> do
---       -- Let's create a new Join.
---       i <- genid
---       let join       = Join i tests amem bmem
---           joinState  = JoinState Nothing []
---           bch        = bmemChildren bmemState
---           bmemState1 = bmemState { bmemChildren = join:bch }
---       rete1 <- get
---       put rete1 { reteJoinStates = Map.insert join joinState
---                                    (reteJoinStates rete1)
---                 , reteBmemStates = Map.insert bmem bmemState1
---                                    (reteBmemStates rete1)}
---       return join
--- {-# INLINE buildOrShareJoin #-}
+      -- Register join as its parent's child.
+      setS parent (over bmemChildren (join:) parentState)
+      return join
+{-# INLINE buildOrShareJoin #-}
